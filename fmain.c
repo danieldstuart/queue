76842f16f398d5e30bc8748e66eeebf4f5de145c
danieldstuart <daniel.d.stuart1@gmail.com>
2015-04-29 20:42:25 -0700

    fmain.c

---
+++ fmain.c
@@ -0,0 +1,17 @@
+#include <queue.h>
+#include <stdio.h>
+
+int main(void){
+	
+	// create queue
+	printf("%d\n", length(cons(32, (cons(42, cons(233, EMPTY))))));
+	
+	// Queue
+		
+	
+	// Dequeue
+	
+	
+	// front
+	return 0;
+}
